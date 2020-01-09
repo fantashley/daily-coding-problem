295f09ddaf5e7713e1e44b1b15684f982c7df4bf
Ashley Nelson <amn@fantashley.com>
2020-01-09 14:22:27 -0600

    Add solution to Problem 1

---
+++ 1-problem/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	numbers := []int{10, 15, 3, 7}
+	k := 17
+
+	fmt.Println(addsToK(numbers, k))
+}
+
+func addsToK(numbers []int, k int) bool {
+	sort.Ints(numbers)
+	for _, n := range numbers {
+		if binarySearch(numbers, 0, len(numbers)-1, k-n) != -1 {
+			return true
+		}
+	}
+	return false
+}
+
+func binarySearch(numbers []int, l int, r int, x int) int {
+
+	for l <= r {
+		mid := (l + r) / 2
+
+		if x < numbers[mid] {
+			return binarySearch(numbers, l, mid-1, x)
+		} else if x > numbers[mid] {
+			return binarySearch(numbers, mid+1, r, x)
+		} else {
+			return numbers[mid]
+		}
+	}
+
+	return -1
+}
