3a09baab79d0c906468c6296c2c2c242967b8ec1
Ashley Nelson <amn@fantashley.com>
2020-01-23 19:14:58 -0600

    Add initial recursive solution for Problem 9

--- 9-problem/main.go
+++ 9-problem/main.go
@@ -1,13 +1,29 @@
 package main
 
+import "fmt"
+
 func main() {
 
-	numbers := []int{2, 4, 6, 2, 5}
-
+	numbers := []int{5, 1, 1, 5}
+	fmt.Println(largestSum(numbers))
 }
 
 func largestSum(numbers []int) int {
 
+	if len(numbers) == 1 {
+		return numbers[0]
+	}
+
+	nonadjacent := numbers[0]
 	adjacent := largestSum(numbers[1:])
-	nonadjacent := largestSum(numbers[2:])
+
+	if len(numbers) > 2 {
+		nonadjacent += largestSum(numbers[2:])
+	}
+
+	if adjacent > nonadjacent {
+		return adjacent
+	}
+
+	return nonadjacent
 }
