4f349d1287e954cc6fb9913f34ef3ef0bc474cc4
Ashley Nelson <fant@shley.email>
2021-07-21 03:30:43 +0000

    Restructure problem directories

---
+++ 0042/main.go
@@ -0,0 +1,20 @@
+package main
+
+/*
+Given a list of integers S and a target number k, write a function that returns
+a subset of S that adds up to k. If such a subset cannot be made, then return
+null.
+
+Integers can appear more than once in the list. You may assume all numbers in
+the list are positive.
+
+For example, given S = [12, 1, 61, 5, 9, 2] and k = 24, return [12, 9, 2, 1]
+since it sums up to 24.
+*/
+
+func main() {
+
+	S := []int{12, 1, 61, 5, 9, 2}
+	k := 24
+
+}
